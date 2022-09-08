ea2fc3ee709f40898f3d21cdb443b83bb847fb8c
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-09-08 11:55:15 -0300

    up

---
+++ Pilhas/pilha.java
@@ -0,0 +1,55 @@
+public class Pilha{
+    int dados[];
+    int topo;
+
+    public Pilha(int capMax){
+        dados = new int[capMax];
+        topo = 0;
+    }
+
+    public boolean cheia(){
+        return dados.length == topo;
+
+    }
+    public boolean vazia(){
+        return topo == 0;
+    }
+    public void empilha(int e){
+        if(cheia()){
+            return false;
+        } else {
+            dados[topo] = e;
+            topo++;
+            return true;
+        }
+    }
+    public int desempilha(){
+        if(vazia()){
+            return null;
+        } else{
+            topo--;
+            return dados[topo];
+        }
+
+    }
+    public void to String(){
+
+    }
+}
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Pilha pl =  new Pilha(3);
+
+        // entrada de dados
+        for(int i = 0; i < dados.length; i++){
+            int num = sc.nextInt();
+            if(pl.cheia()){
+                System.out.println("ERRO! Pilha Cheia.");
+            } else{
+                pl.empilha(num);
+            }
+        }
+    }
+}
