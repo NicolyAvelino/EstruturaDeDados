d77275963333739913bf4b243e47f799c4e3f0b0
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-10-24 12:47:48 -0300

    recursividade

--- Recursividade/fat&som.java
+++ Recursividade/fat&som.java
@@ -25,12 +25,48 @@ public class Inteiro{
             return 0;
         return soma3(n/2) + 1.0/n;
     }
+
+    // mdc
+    public int mdc(int v1, int v2){
+        int r = v1 % v2;
+        if(r==0)
+            return v2;
+        return mdc(v2,r);
+    }
+
+    // fibonacci
+    public int fibonacci(int n){
+        if(n == 0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    // fibonacci usando vetor
+    public void fibonacciV(int vet[], int n){
+        if(n==1){
+            vet[n-1] = 1;
+        }
+        else if(n==2){
+            vet[n-1] = 1;
+        }
+        else{
+            fibonacciV(vet, n-1);
+            vet[n-1] = vet[n-2] + vet[n-3];
+        }
+        return fibonacciV(n-1) + fibonacciV(n-2);
+    }
 }
 
 import java.util.Scanner;
 public class Main{
     public static void main(String args[]){
         Inteiro inteiro = new Inteiro();
+        int t = 5;
+        int vet = new int[5];
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite um valor: ");
@@ -40,7 +76,14 @@ public class Main{
         int sm = inteiro.soma(n);
         double sm2 = inteiro.soma2(n);
         double sm3 = inteiro.soma3(n);
+
+        // mdc
+        int mdc = inteiro.mdc(250,150);
+
+        // fibonacci
+        int fb = inteiro.fibonacci(n);
+        int fbV = inteiro.fibonacciV(vet,t);
         
-        System.out.printf("%d! = %d \nSomatória = %d \nSomatória 2º = %.2f \nSomatória 3º = %.2f",n,ft,sm,sm2,sm3);
+        System.out.printf("%d! = %d \nSomatória = %d \nSomatória 2º = %.2f \nSomatória 3º = %.2f \nMDC = %d \nFibonacci = %d \nFibonacci Com Vetor = %d",n,ft,sm,sm2,sm3,mdc,fb,fbV);
     }
 }
